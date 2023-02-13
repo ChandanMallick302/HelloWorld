b98e7b3304a699a320e277b7454f28c104725232
ChandanMallick302 <chandanmallick302@gmail.com>
2023-02-13 18:09:43 +0530

    First Commit

---
+++ Practice/src/com/simplilearn/Array1.java
@@ -0,0 +1,12 @@
+package com.simplilearn;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+
+		int a[] = { 1, 2, 3 };
+		int b[] = { 1, 2, 3 };
+		System.out.println(a[1] + b[2]);
+		System.out.println("Hii");
+	}
+}
